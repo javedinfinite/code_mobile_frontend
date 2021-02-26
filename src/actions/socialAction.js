0f29257fbcd1aba2c9ff71cdc533ee404b0a7f89
Javed <javedaktar73@gmail.com>
2021-02-26 22:04:55 +0530

    added heroku api base url

--- src/actions/socialAction.js
+++ src/actions/socialAction.js
@@ -1,7 +1,8 @@
 import Actions from '../actionConstants/socialActionConstants';
 import axios from 'axios';
 
-const base_url = 'http://localhost:4000'
+// const base_url = 'http://localhost:4000'
+const base_url = 'https://codemymobile.herokuapp.com'
 
 export const getusers = () => {
   return async (dispatch) => {
